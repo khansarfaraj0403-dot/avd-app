9a35a73413e161dfc2a08c3ef7326bd200fd8911
Sarfaraj khan <khansarfaraj0403@gmail.com>
2026-05-19 20:56:11 +0530

    test 2

--- test_cal.py
+++ test_cal.py
@@ -4,7 +4,7 @@ def test_add():
     assert add(10, 20) == 30
 def test_sub():
     assert sub(10, 5) == 5
-# def test_mul():
-#     assert mul(10, 20) == 200
-# def test_div():
-#     assert div(10, 2) == 5
+def test_mul():
+    assert mul(10, 20) == 200
+def test_div():
+    assert div(10, 2) == 5
